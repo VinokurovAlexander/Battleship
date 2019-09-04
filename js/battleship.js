88d1bb535e69f9cc6605725e33252b6170f815f1
Alexander <vinokurov.ag@mail.ru>
2019-09-04 15:00:08 +0300

    Добавлен объект 'Контроллер' отвечающий за обработку координат выстрела и передачу их в 'Модель'

--- js/battleship.js
+++ js/battleship.js
@@ -53,41 +53,56 @@ var model = {
 
   isSunk: function(ship) {
     for (var i = 0; i < this.shipLength; i++) {
-      if (ship.hits[i] !== "hits") {
+      if (ship.hits[i] !== "hit") {
         return false;
       }
     }
     return true;
   }
-
 };
 
 var controller = {
   guesses: 0,
-  parseGuess: function (guess) {
-    var alphabet = ["A","B","C","D","E","F","G"];
-
-    if (guess === null || guess.length !== 2) {
-      alert("Oops, please enter a letter and a number on the board.");
-    } else {
-      firstChar = guess.charAt(0);
-      var row = alphabet.indexOf(firstChar);
-      var column = guess.charAt(1);
-
-      if (isNaN(row) || isNaN(column)) {
-        alert("Oops, that isn't on the board.");
-      } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
-        alert("Oops, that's off the board!");
-      } else {
-        return row + column;
+  processGuess: function(guess) {
+    var location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+      var hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
       }
     }
-    return null;
   }
 };
 
-console.log(controller.parseGuess("A0"));
-console.log(controller.parseGuess("B6"));
-console.log(controller.parseGuess("G3"));
-console.log(controller.parseGuess("H0"));
-console.log(controller.parseGuess("A7"));
+function parseGuess(guess) {
+  var alphabet = ["A","B","C","D","E","F","G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.");
+  } else {
+    firstChar = guess.charAt(0);
+    var row = alphabet.indexOf(firstChar);
+    var column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+      alert("Oops, that's off the board!");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+};
+
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
